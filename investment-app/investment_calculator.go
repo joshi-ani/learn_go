0a7b4fda918f7ac443882bf580e54cfcaa1330be
Aniruddha Joshi <aniruddha.joshi@sas.com>
2024-06-25 14:30:34 +0530

    learn-go exercises - profit calculator and investment app

--- investment-app/investment_calculator.go
+++ investment-app/investment_calculator.go
@@ -6,10 +6,23 @@ import (
 )
 
 func main() {
-	investmentAmount, years := 1000.0, 10.0
-	expectedReturnRate := 5.5
+	const inflationRate = 6.5
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 	// var years float64 = 10
 
+	fmt.Print("Enter the investment amount: ")
+	// Use pointer to store the user input value in the investmentAmount variable
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter the years: ")
+	fmt.Scan(&years)
+
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
